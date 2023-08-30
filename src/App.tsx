8439c2b5371609d91ac3d195de7e0de15524df32
ihccenen <wirkz01@gmail.com>
2023-08-30 12:45:14 -0300

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+
 export default function App() {
-  return <main></main>;
+  return (
+    <main>
+      <Header />
+      <Outlet />
+    </main>
+  );
 }
