19c36813091ef29ce5d7354bd7eaaff7ce525a61
ihccenen <wirkz01@gmail.com>
2023-08-30 12:45:25 -0300

    added error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import App from './App';
+import ErrorPage from './routes/ErrorPage';
+import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
+    children: [],
   },
 ]);
 
