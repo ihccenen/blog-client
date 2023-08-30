8be6a75a22e19a65176a68d5d23e8097f3fff3ca
ihccenen <wirkz01@gmail.com>
2023-08-30 15:01:46 -0300

    added class name

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function ErrorPage() {
   return (
-    <main className="main flex">
+    <main className="main error-page flex">
       <h1>Oh no, this route doesn't exist!</h1>
       <Link to="/">
         You can go back to the home page by clicking here, though!
